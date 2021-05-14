d9a6095e7ea9af143d087daaab98880f5560bc64
Ashish Ahuja <ashishahuja124@gmail.com>
2021-05-14 16:01:40 +0530

    trust issue with setTimeout due to blocking of code

--- index.js
+++ index.js
@@ -196,15 +196,31 @@
 
 //Callback functions
 
-setTimeout(function() {
-    console.log('timer');
+// setTimeout(function() {
+//     console.log('timer');
+// },5000);
+
+// function x(y) {
+//     console.log('x');
+//     y();
+// }
+
+// x(function y() {
+//     console.log('y');
+// });
+
+console.log('star');
+setTimeout(function(cb) {
+    console.log('cb')
 },5000);
 
-function x(y) {
-    console.log('x');
-    y();
+console.log('end');
+
+let startDate = new Date().getTime();
+let endDate = startDate;
+
+while(endDate < startDate + 10000) {
+    endDate = new Date().getTime();
 }
 
-x(function y() {
-    console.log('y');
-});
+console.log('while expires and blocking the code');
